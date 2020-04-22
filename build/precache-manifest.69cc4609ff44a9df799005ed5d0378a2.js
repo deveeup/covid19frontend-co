7e5c7c088eeec47c4251d0d127fbf7457727563c
David R <deveeup@MacBook-Pro.local>
2020-04-22 11:54:13 -0500

    change folder build

---
+++ build/precache-manifest.69cc4609ff44a9df799005ed5d0378a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0af30ee35d9b1d46756ce5f834005131",
+    "url": "/index.html"
+  },
+  {
+    "revision": "044da160030cf8a511c8",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "04119e21bdeadeb03f21",
+    "url": "/static/js/2.da36c16b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.da36c16b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "044da160030cf8a511c8",
+    "url": "/static/js/main.9a033594.chunk.js"
+  },
+  {
+    "revision": "17dcb9a66dd772febb8d",
+    "url": "/static/js/runtime-main.904b8476.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
