286aba7b949ce35b4e4010f622fd0c063583f1bf
David R <deveeup@MacBook-Pro.local>
2020-04-23 14:32:44 -0500

    clear structure

---
+++ build/precache-manifest.a1653b3bed5d0b8db3ae42e5a8f80986.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12d8f2adb3f0d1bd52d70724e6aa5f26",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ed9723cd0eb9eeeaf93a",
+    "url": "./static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "04119e21bdeadeb03f21",
+    "url": "./static/js/2.da36c16b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.da36c16b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed9723cd0eb9eeeaf93a",
+    "url": "./static/js/main.73b823ea.chunk.js"
+  },
+  {
+    "revision": "1de5a5f2abfae5725c11",
+    "url": "./static/js/runtime-main.70b9fa85.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
