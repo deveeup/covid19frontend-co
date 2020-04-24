3505017d174048c0b0a8223e3d959377c8020e97
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-24 01:03:10 +0000

    Deploy deveeup/covid19frontend-co to github.com/deveeup/covid19frontend-co.git:gh-pages

---
+++ precache-manifest.cafbba75600c07a897cb7889ea9c5c8a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dc3b6c11993a0b288acf46cd97cfaa4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6dac205bc7e1474bebd3",
+    "url": "./static/css/main.2532210b.chunk.css"
+  },
+  {
+    "revision": "a30ae55cf4752aa336f7",
+    "url": "./static/js/2.ad146fc9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.ad146fc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6dac205bc7e1474bebd3",
+    "url": "./static/js/main.e9fbb5f9.chunk.js"
+  },
+  {
+    "revision": "1de5a5f2abfae5725c11",
+    "url": "./static/js/runtime-main.70b9fa85.js"
+  },
+  {
+    "revision": "22567d3d05467c2362f8c89c65afb336",
+    "url": "./static/media/covid19col.22567d3d.svg"
+  },
+  {
+    "revision": "fd471c8af43082ab4e2b4d143cd35e51",
+    "url": "./static/media/elbow.fd471c8a.svg"
+  },
+  {
+    "revision": "c259733c8f8d7635d8a63b6df8485842",
+    "url": "./static/media/home.c259733c.svg"
+  },
+  {
+    "revision": "2bbc42111402ba504b30a8705b77c35a",
+    "url": "./static/media/info.2bbc4211.svg"
+  },
+  {
+    "revision": "d80e565dc752e030b81d5b2504d0271b",
+    "url": "./static/media/map.d80e565d.svg"
+  },
+  {
+    "revision": "3111642eba480b3887944d758f2a9912",
+    "url": "./static/media/sick.3111642e.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.a1653b3bed5d0b8db3ae42e5a8f80986.js"
+  "./precache-manifest.cafbba75600c07a897cb7889ea9c5c8a.js"
 );
 
 self.addEventListener('message', (event) => {
