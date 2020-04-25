e4dd0461038cb74f9bca62bbd5bc3eeddad3b204
David R <deveeup@MacBook-Pro.local>
2020-04-25 15:46:23 -0500

    disable hook

--- src/components/pages/home/index.js
+++ src/components/pages/home/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect } from "react";
 import InfoCol from "./info-col";
 import InfoColDetail from "./info-col-detail";
