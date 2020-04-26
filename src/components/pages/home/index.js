da7dc3d58f17db52188635bc5de22e2eb642e71e
David R <deveeup@MacBook-Pro.local>
2020-04-25 20:49:58 -0500

    loading

--- src/components/pages/home/index.js
+++ src/components/pages/home/index.js
@@ -11,11 +11,7 @@ import "./index.scss";
 const Home = () => {
   const PROXY_URL = process.env.REACT_APP_URL_CORS;
   const API = `${process.env.REACT_APP_URL_API_COL}/25-04-2020`;
-  const { colombia } = useSelector((state) => state.colombia);
-  const [status, setStatus] = useState({
-    load: true,
-    error: false,
-  });
+  const { data, loading } = useSelector((state) => state.colombia);
   const dispatch = useDispatch();
   const addData = useCallback(
     (data) => {
@@ -24,26 +20,26 @@ const Home = () => {
     [dispatch]
   );
   useEffect(() => {
-    (async () => {
-      await axios.get(PROXY_URL + API).then((response) => {
-        const convertData = JSON.stringify(response.data.data.data);
-        addData(JSON.parse(convertData));
-        setStatus({
-          load: false,
-          error: false,
+    if (!data) {
+      (async () => {
+        await axios.get(PROXY_URL + API).then((response) => {
+          const convertData = JSON.stringify(response.data.data.data);
+          addData({
+            data: JSON.parse(convertData),
+            loading: false,
+          });
         });
-      });
-    })();
+      })();
+    }
   }, []);
-
-  if (status.load) {
+  if (loading) {
     return <h1>cargando</h1>;
   } else {
     return (
       <div className="Home">
         <h2>Colombia</h2>
-        <InfoCol total={colombia.total} />
-        <InfoColDetail details={colombia} />
+        <InfoCol total={data.total} />
+        <InfoColDetail details={data} />
         <h2>Información de casos por departamento</h2>
         <InfoDepartaments />
       </div>
--- src/reducers/colombia.js
+++ src/reducers/colombia.js
@@ -1,10 +1,13 @@
-const initialState = {};
+const initialState = {
+  data: null,
+  loading: true,
+};
 export default function (state = initialState, action) {
   switch (action.type) {
     case "ADD_DATA":
       return {
         ...state,
-        colombia: action.payload,
+        ...action.payload,
       };
     case "REMOVE_DATA":
       return {
